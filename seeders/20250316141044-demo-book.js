6b5721dbd9f6cbadc9ebbe0b228cca3afc60fb92
Muhamad Imam Catur <mimamc95@gmail.com>
2025-03-16 21:18:36 +0700

    create seeder using sequelize-cli

---
+++ seeders/20250316141044-demo-book.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+
+    return queryInterface.bulkInsert('books', [
+      {
+        title: 'Node JS',
+        description: 'Learning Node JS for beginner',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('books', null, {});
+  }
+};
